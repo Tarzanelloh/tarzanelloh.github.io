ee844d026a50bdef15d94feecb683f3fde478ff1
Luca <lmenghini@coders51.com>
2021-10-15 17:37:58 +0200

    fix

--- wf-auth0-integration.js
+++ wf-auth0-integration.js
@@ -41,7 +41,7 @@ const attachListeners = () => {
     logoutButtons.forEach(lb => {
         lb.addEventListener('click', () => {
             auth0.logout({
-                returnTo: window.location.origin
+                returnTo: window.location.href
             });
         })
     })
