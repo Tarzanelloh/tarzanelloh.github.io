f5a3368e91d3d0b744ce2b61b7bf55d76eade606
Luca <lmenghini@coders51.com>
2021-12-03 17:47:38 +0100

    test

--- test.js
+++ test.js
@@ -1,3 +1,3 @@
-import("https://cdn.auth0.com/js/auth0-spa-js/1.18/auth0-spa-js.production.js").then(function(module) {
-    console.log("hi from auth0?", auth0)
-});
+import auth0 from "https://cdn.auth0.com/js/auth0-spa-js/1.18/auth0-spa-js.production.js"
+
+console.log("hi from auth0?", auth0)
