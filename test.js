434da59ca00abe6d1a45f86404e0682d5b50b93d
Luca <lmenghini@coders51.com>
2021-12-03 18:06:58 +0100

    test

--- test.js
+++ test.js
@@ -1,3 +1,13 @@
 injectScript("https://cdn.auth0.com/js/auth0-spa-js/1.18/auth0-spa-js.production.js").then(() => {
+    const config = {
+        auth0: {
+            domain: "uhubs-staging.eu.auth0.com",
+            client_id: "RLaFcdBuvgXPws43E3iQkjYPCqMeR4Tq",
+            cacheLocation: "localstorage",
+            audience: "https://api.uhubs.co.uk"
+        },
+        backend: "https://api-staging.uhubs.co.uk"
+    }
+    auth0 = await createAuth0Client(config.auth0);
     console.log("hi from auth0?", auth0)
 });
