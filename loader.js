42306363db0f829b396de9cedf528dd717fecfec
Luca <lmenghini@coders51.com>
2021-12-03 17:55:04 +0100

    loader

---
+++ loader.js
@@ -0,0 +1,9 @@
+function injectScript(src) {
+    return new Promise((resolve, reject) => {
+        const script = document.createElement('script');
+        script.src = src;
+        script.addEventListener('load', resolve);
+        script.addEventListener('error', e => reject(e.error));
+        document.head.appendChild(script);
+    });
+}
